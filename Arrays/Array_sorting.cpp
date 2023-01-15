a6059c7a4bf12409aeccf9731d82d4fe77a85579
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-01-15 18:06:46 +0000

    Add files via upload

---
+++ Arrays/Array_sorting.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+ 
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+ 
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+}
+ 
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+int main()
+{
+    int arr[] = { 5, 1, 4, 2, 8 };
+    cout<<"Unsorted array"<<endl;
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    cout<<endl;
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
